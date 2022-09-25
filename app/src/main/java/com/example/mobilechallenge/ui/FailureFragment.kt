c0e86771146c19e8f9297eafef7b2ea5e281e062
brnguyen <canhnv@gmail.com>
2022-09-25 22:51:19 +0100

    Implement Payment success and failure fragment

---
+++ app/src/main/java/com/example/mobilechallenge/ui/FailureFragment.kt
@@ -0,0 +1,24 @@
+package com.example.mobilechallenge.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mobilechallenge.R
+
+class FailureFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_failure, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            FailureFragment()
+    }
+}
