f5b4b1811534d1d84414def718671f465f32966f
brnguyen <canhnv@gmail.com>
2022-09-25 19:52:05 +0100

    Move MainActivity to 'ui' package

--- app/src/main/java/com/example/mobilechallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/mobilechallenge/ui/MainActivity.kt
@@ -1,17 +1,22 @@
-package com.example.mobilechallenge
+package com.example.mobilechallenge.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.mobilechallenge.R
+import com.example.mobilechallenge.api.ApiStatus
 import com.example.mobilechallenge.databinding.ActivityMainBinding
 import com.example.mobilechallenge.utils.formatter.CardNumFormatter
 import com.example.mobilechallenge.utils.formatter.CvvFormatter
 import com.example.mobilechallenge.utils.formatter.ExpiryDateFormatter
 import com.example.mobilechallenge.utils.formatter.getPureNumber
 import com.example.mobilechallenge.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -34,10 +39,28 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.payButton.setOnClickListener {
-            val cardNum = binding.cardNumberEditText.getPureNumber()
-            val expiryDate = binding.cardDateEditText.text.toString()
-            val cvv = binding.cardCVCEditText.text.toString()
-            viewModel.updateCardInfo(cardNum, expiryDate, cvv)
+            onPayClicked()
         }
+    }
+
+    private fun onPayClicked() {
+        val cardNum = binding.cardNumberEditText.getPureNumber()
+        val expiryDate = binding.cardDateEditText.text.toString()
+        val cvv = binding.cardCVCEditText.text.toString()
+        viewModel.updateCardInfo(cardNum, expiryDate, cvv)
+        if (viewModel.getCardInfo()?.isValid() == true) {
+            lifecycleScope.launch {
+                val result = viewModel.submitPay(this@MainActivity)
+                if (result.status == ApiStatus.SUCCESS) {
+
+                } else {
+                    showErrorDialog(result.message)
+                }
+            }
+        }
+    }
+
+    private fun showErrorDialog(message: String?) {
+        AlertDialog.Builder(this).setMessage(getString(R.string.pay_error_message, message)).show()
     }
 }
