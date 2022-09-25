3910501ed017475d2f5e2c68ed8fdd929ed54910
brnguyen <canhnv@gmail.com>
2022-09-25 19:48:44 +0100

    Implement submitPay API in MainViewModel

--- app/src/main/java/com/example/mobilechallenge/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mobilechallenge/viewmodel/MainViewModel.kt
@@ -1,28 +1,52 @@
 package com.example.mobilechallenge.viewmodel
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mobilechallenge.R
+import com.example.mobilechallenge.api.ApiResult
 import com.example.mobilechallenge.model.CardInfo
-import com.example.mobilechallenge.utils.formatter.TextFormatter
+import com.example.mobilechallenge.payment.PaymentHelper
 
 class MainViewModel : ViewModel() {
-    private val cardInfoLiveData = MutableLiveData<CardInfo>()
+    private var cardInfo: CardInfo? = null
     private val isCardValidLiveData = MutableLiveData<Boolean>().apply { postValue(true) }
+    private val isUiBusyLiveData = MutableLiveData<Boolean>().apply { postValue(false) }
 
-    fun getCardInfo(): LiveData<CardInfo> {
-        return cardInfoLiveData
+    companion object {
+        private const val TAG = "MainViewModel"
+    }
+
+    fun getCardInfo(): CardInfo? {
+        return cardInfo
     }
 
     fun updateCardInfo(cardNum: String, expiryDate: String, cvv: String) {
-        val cardInfo = CardInfo(cardNum, expiryDate, cvv)
-        cardInfoLiveData.postValue(cardInfo)
-        isCardValidLiveData.postValue(cardInfo.isValid())
+        cardInfo = CardInfo(cardNum, expiryDate, cvv)
+        isCardValidLiveData.postValue(cardInfo?.isValid())
     }
 
     fun isCardValid(): LiveData<Boolean> {
         return isCardValidLiveData
     }
 
+    fun isUiBusy(): LiveData<Boolean> {
+        return isUiBusyLiveData
+    }
 
+    suspend fun submitPay(context: Context): ApiResult<String> {
+        val submitCardInfo = cardInfo
+        return if (submitCardInfo == null || !submitCardInfo.isValid()) {
+            Log.e(TAG, "Card info is null or not valid")
+            ApiResult.Error(context.getString(R.string.pay_error_message_card_invalid))
+        } else {
+            isUiBusyLiveData.postValue(true)
+            val result = PaymentHelper.submitPay(submitCardInfo)
+            Log.i(TAG, "response Url = $result")
+            isUiBusyLiveData.postValue(false)
+            result
+        }
+    }
 }
