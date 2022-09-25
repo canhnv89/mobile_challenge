013c6a2ab6b688f946feb325ae370e2d86e5fa84
brnguyen <canhnv@gmail.com>
2022-09-25 17:38:40 +0100

    Add view model to Main Activity

--- app/src/main/java/com/example/mobilechallenge/MainActivity.kt
+++ app/src/main/java/com/example/mobilechallenge/MainActivity.kt
@@ -5,20 +5,20 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
+import androidx.lifecycle.ViewModelProvider
 import com.example.mobilechallenge.databinding.ActivityMainBinding
 import com.example.mobilechallenge.utils.formatter.CardNumFormatter
 import com.example.mobilechallenge.utils.formatter.CvvFormatter
 import com.example.mobilechallenge.utils.formatter.ExpiryDateFormatter
+import com.example.mobilechallenge.utils.formatter.getPureNumber
+import com.example.mobilechallenge.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var cardNumFormatter: CardNumFormatter
     private lateinit var expiryDateFormatter: ExpiryDateFormatter
     private lateinit var cvvFormatter: CvvFormatter
-
-    companion object {
-
-    }
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +29,15 @@ class MainActivity : AppCompatActivity() {
         cardNumFormatter = CardNumFormatter(binding.cardNumberEditText)
         expiryDateFormatter = ExpiryDateFormatter(binding.cardDateEditText)
         cvvFormatter = CvvFormatter(binding.cardCVCEditText)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        binding.payButton.setOnClickListener {
+            val cardNum = binding.cardNumberEditText.getPureNumber()
+            val expiryDate = binding.cardDateEditText.text.toString()
+            val cvv = binding.cardCVCEditText.text.toString()
+            viewModel.updateCardInfo(cardNum, expiryDate, cvv)
+        }
     }
 }
